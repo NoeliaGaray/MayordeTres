f734f3258e5da5c2134ae7b39fb670e26da13662
NoeliaGaray <103612855+NoeliaGaray@users.noreply.github.com>
2022-05-13 21:36:41 -0300

    listo

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,18 @@ rotulo2.innerHTML = "Ingresar número";
 rotulo3.innerHTML = "Ingresar número";
 
 let btnEnv = document.getElementById("btnEnviar");
-let número1: number = Number(dato1.value);
-let número2: number = Number(dato2.value);
-let número3: number = Number(dato3.value);
 
 btnEnv.addEventListener("click", () => {
-  let btnEnv = document.getElementById("btnEnviar");
   let número1: number = Number(dato1.value);
   let número2: number = Number(dato2.value);
   let número3: number = Number(dato3.value);
-  if (número1 >= número2 && número1 >= número3) {
+  if (número1 >= número2) {
     console.log("El número mayor es: ", número1);
-  } else {
+  } else if (número1 >= número3) {
     console.log("El número mayor es: ", número2);
+  } else if (número2 >= número3) {
+    console.log("El número mayor es: ", número2);
+  } else {
+    console.log("El número mayor es: ", número3);
   }
 });
